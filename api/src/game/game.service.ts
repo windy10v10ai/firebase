50415969e7fea824c4bf78859dac6298541ec5d7
windy <windybirth@gmail.com>
2024-11-13 09:07:38 +0900

    fix

--- api/src/game/game.service.ts
+++ api/src/game/game.service.ts
@@ -1,8 +1,4 @@
-import {
-  BadRequestException,
-  Injectable,
-  UnauthorizedException,
-} from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { logger } from 'firebase-functions';
 
 import { EventRewardsService } from '../event-rewards/event-rewards.service';
