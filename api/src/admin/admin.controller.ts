0b634678c84cb51cd482b7be7e1f63dcb801da4c
windy <windybirth@gmail.com>
2024-12-15 13:13:35 +0900

    Update admin.controller.ts

--- api/src/admin/admin.controller.ts
+++ api/src/admin/admin.controller.ts
@@ -1,14 +1,15 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 
-import { ResetSeasonPoint } from '../player/dto/reset-season-point.dto';
 import { PlayerService } from '../player/player.service';
 import { PlayerPropertyService } from '../player-property/player-property.service';
+import { Public } from '../util/auth/public.decorator';
 
 import { AdminService } from './admin.service';
 import { CreateAfdianMemberDto } from './dto/create-afdian-member.dto';
 import { CreatePatreonMemberDto } from './dto/create-patreon-member.dto';
 
+@Public()
 @ApiTags('Admin')
 @Controller('admin')
 export class AdminController {
@@ -26,10 +27,5 @@ export class AdminController {
   @Post('/member/patreon')
   createPatreonMember(@Body() createPatreonMemberDto: CreatePatreonMemberDto) {
     return this.adminService.createPatreonMember(createPatreonMemberDto);
-  }
-
-  @Post('/players/season/resetSeasonPoint')
-  resetSeasonPoint(@Body() resetSeasonPoint: ResetSeasonPoint) {
-    return this.playerService.resetSeasonPoint(resetSeasonPoint.resetPercent);
   }
 }
