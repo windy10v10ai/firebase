eee0943f5d25ba2d7ca68be2df5e13823489cf37
windy <windybirth@gmail.com>
2024-12-16 22:43:58 +0900

    fix functions

--- api/src/app.controller.ts
+++ api/src/app.controller.ts
@@ -1,6 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
-import * as functions from 'firebase-functions';
 
 import { AppService } from './app.service';
 import { Public } from './util/auth/public.decorator';
@@ -14,11 +13,5 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
-  }
-
-  @Get('env')
-  getEnv(): string {
-    const test_env = functions.config().admin?.test_env;
-    return `test_env: ${test_env}`;
   }
 }
