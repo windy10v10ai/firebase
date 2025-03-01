259250f5085a2a36baf549b4288a16633439f994
windy <windybirth@gmail.com>
2025-03-01 18:03:49 +0900

    impl

--- api/src/player/player.module.ts
+++ api/src/player/player.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { FireormModule } from 'nestjs-fireorm';
 
+import { AnalyticsModule } from '../analytics/analytics.module';
+
 import { Player } from './entities/player.entity';
 import { PlayerController } from './player.controller';
 import { PlayerService } from './player.service';
 
 @Module({
-  imports: [FireormModule.forFeature([Player])],
+  imports: [FireormModule.forFeature([Player]), AnalyticsModule],
   controllers: [PlayerController],
   providers: [PlayerService],
   exports: [PlayerService],
