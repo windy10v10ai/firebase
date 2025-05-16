b1efd49ca70ca54b2d21c4360ace29c97e2c2e59
diplo <wxzclc@gmail.com>
2025-05-16 20:37:42 +0800

    网页激活ko-fi会员 (#550)
    * ko-fi手动激活会员-前台页面
    
    * 修改参数名
    
    * fix bug，失败时页面显示
    
    * kofi en
    
    * Update kofi.service.ts
    
    ---------
    
    Co-authored-by: windy <windybirth@gmail.com>

--- web/config/constant.ts
+++ web/config/constant.ts
@@ -1,3 +1,4 @@
 export const API_DOMAIN = process.env.API_DOMAIN;
 
 export const afdianActiveUrl = `${API_DOMAIN}/api/afdian/order/active`;
+export const kofiActiveUrl = `${API_DOMAIN}/api/kofi/order/active`;
