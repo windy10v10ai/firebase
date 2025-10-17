5bb55779480c1cef8610b8f756462b3b5e289659
windy <windybirth@gmail.com>
2025-10-17 10:01:27 +0900

    Add server type tracking to analytics events

--- api/src/util/secret/secret.service.ts
+++ api/src/util/secret/secret.service.ts
@@ -1,6 +1,14 @@
 import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { logger } from 'firebase-functions';
 
+export enum SERVER_TYPE {
+  PRODUCTION = 'PRODUCTION',
+  TEST = 'TEST',
+  TENVTEN = 'TENVTEN',
+  LOCAL = 'LOCAL',
+  UNKNOWN = 'UNKNOWN',
+}
+
 export enum SECRET {
   AFDIAN_WEBHOOK_TOKEN = 'AFDIAN_WEBHOOK_TOKEN',
   AFDIAN_API_TOKEN = 'AFDIAN_API_TOKEN',
@@ -20,5 +28,26 @@ export class SecretService {
       throw new InternalServerErrorException('Secret value is not defined');
     }
     return value;
+  }
+
+  getServerTypeByApiKey(apiKey: string): SERVER_TYPE {
+    if (apiKey === 'Invalid_NotOnDedicatedServer') {
+      return SERVER_TYPE.LOCAL;
+    }
+
+    const productionKey = this.getSecretValue(SECRET.SERVER_APIKEY);
+    if (apiKey === productionKey) {
+      return SERVER_TYPE.PRODUCTION;
+    }
+    const testKey = this.getSecretValue(SECRET.SERVER_APIKEY_TEST);
+    if (apiKey === testKey) {
+      return SERVER_TYPE.TEST;
+    }
+    const tenvtenKey = this.getSecretValue(SECRET.SERVER_APIKEY_TENVTEN);
+    if (apiKey === tenvtenKey) {
+      return SERVER_TYPE.TENVTEN;
+    }
+
+    return SERVER_TYPE.UNKNOWN;
   }
 }
