3cf9576bf242b32f36dcbd0407248ec4473330e1
windy <windybirth@gmail.com>
2026-05-07 18:20:58 +0900

    Update player-property.module.ts

--- api/src/player-property/player-property.module.ts
+++ api/src/player-property/player-property.module.ts
@@ -4,9 +4,6 @@ import { FireormModule } from 'nestjs-fireorm';
 import { PlayerProperty } from './entities/player-property.entity';
 import { PlayerPropertyService } from './player-property.service';
 
-/**
- * @deprecated 此 Module 将在未来版本中移除，请使用 PlayerPropertyV2Module 替代
- */
 @Module({
   imports: [FireormModule.forFeature([PlayerProperty])],
   controllers: [],
