c07dab650c766e199a41fde1d7d40f0a8d53ed6f
windy <windybirth@gmail.com>
2026-08-15 19:28:36 +0900

    feat(daily-task): add module skeleton and shared enums

---
+++ api/src/daily-task/types/daily-task.types.ts
@@ -0,0 +1,17 @@
+export enum TaskScope {
+  PERSONAL_GENERAL = 'personal_general',
+  PERSONAL_HERO = 'personal_hero',
+}
+
+export enum TaskMetric {
+  KILLS = 'kills',
+  ASSISTS = 'assists',
+  LAST_HITS = 'last_hits',
+  TOWER_KILLS = 'tower_kills',
+  HERO_DAMAGE = 'hero_damage',
+  HEALING = 'healing',
+  TOTAL_GOLD_EARNED = 'total_gold_earned',
+  DAMAGE_TAKEN = 'damage_taken',
+  STUN_DURATION = 'stun_duration',
+  ROSHAN_KILLS = 'roshan_kills',
+}
